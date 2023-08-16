01848eae82415ecc443d0cc0a4fdb817b7939896
Mau1712 <109633882+Mau1712@users.noreply.github.com>
2023-08-16 02:36:54 -0300

    actApp

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-
-
 import MiNav from './components/nav/NavBar';
 
 function App() {
